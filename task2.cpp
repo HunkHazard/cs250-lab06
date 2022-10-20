5c10d206e61e89fd82e243c0db1ed3717a462538
Jawad <jkabir789@gmail.com>
2022-10-20 22:11:10 +0500

    Task 2: Third Commit

--- task2.cpp
+++ task2.cpp
@@ -23,25 +23,40 @@ void display(vector<int> v){
     cout << endl;
 }
 
-//simple bubble sort algorithm implementation
+//sorting an almost sorted vector using a single loop
 vector<int> moveMin(vector<int> v){
     int temp;
     for (int i = 0; i < v.size(); i++){
-        for (int j = i; j < v.size(); j++){
-            if(v[i]>v[j]){
+        //the final element gets compared with the element of current index and gets interchanged if it is smaller
+        if(v[(v.size())-1] < v[i]){
                 temp = v[i];
-                v[i] = v[j];
-                v[j] = temp;
+                v[i] = v[(v.size())-1];
+                v[(v.size())-1] = temp;
             }
         }
-    }
     return v;
 }
+
+//simple bubble sort algorithm implementation
+// vector<int> moveMin(vector<int> v){
+//     int temp;
+//     for (int i = 0; i < v.size(); i++){
+//         for (int j = i; j < v.size(); j++){
+//             if(v[i]>v[j]){
+//                 temp = v[i];
+//                 v[i] = v[j];
+//                 v[j] = temp;
+//             }
+//         }
+//     }
+//     return v;
+// }
+
 
 bool testMoveMin(){
     //the original test vector gets initialized with random generated values
     vector<int> v_in;
-    v_in = vectorGenerator(10);
+    v_in = vectorGenerator(10000);
     cout << "Generated Vector:\n";
     display(v_in);
 
@@ -64,8 +79,19 @@ bool testMoveMin(){
     display(v_in);
 
     //the dummy vector is then sorted using our fucntion (moveMin())
+    //the time taken by both algos will be measured here
+    // N = 100    Bubble-Sort =  4.9e-05 sec   New Sort = 5e-06 sec
+    // N = 1000   Bubble-Sort =  0.005651 sec  New Sort = 3.3e-05
+    // N = 10000  Bubble-Sort =  0.432444 sec  New Sort = 0.000119
+    clock_t start,stop;
     cout << "Out-Vector after Implemented Function Sorting:\n";
+
+    start = clock();
     v_out = moveMin(v_out);
+    stop = clock();
+
+    double time_taken = double(stop - start)/double(CLOCKS_PER_SEC);
+    cout << "Time Taken by Algorithm : " << time_taken << endl;
     display(v_out);
 
     //then we check whether the origial sorted vector is the same as the sorted dummy vector (using the implemented function)
